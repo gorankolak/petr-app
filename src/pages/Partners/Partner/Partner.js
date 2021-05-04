12b0fcf221f671d8bdeef4c3861e3e1e009ab291
Goran Kolak <goran.kolak82@gmail.com>
2021-05-04 08:38:17 +0200

    Partners invoices list display on partner page

--- src/pages/Partners/Partner/Partner.js
+++ src/pages/Partners/Partner/Partner.js
@@ -1,25 +1,78 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
+import db from '../../../services/db';
 
+import { DefaultTable } from '../../../components/Table/Table';
 import { MainFooter } from '../../../components/mainFooter/mainFooter';
 import { Button } from '../../../components/Components';
 import PartnerStyle from './PartnerStyle';
 
 const Partner = (props) => {
   const [partner, setPartner] = useState([]);
+  const [partnerInvoices, setPartnerInvoices] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
     const getPartners = async () => {
       const partnerData = props.location.state.row.values;
 
-      console.log(partnerData);
+      const invoices = await db.invoices
+        .where('partner')
+        .equals(partnerData.name)
+        .toArray();
 
+      console.log(invoices);
+
+      setPartnerInvoices(invoices);
       setPartner(partnerData);
     };
 
     getPartners();
   }, []);
+
+  const COLUMNS = [
+    {
+      Header: 'Br.',
+      accessor: 'invoiceNumber',
+    },
+    {
+      Header: 'Kupac',
+      accessor: 'partner',
+    },
+    {
+      Header: 'Datum izdavanja',
+      accessor: 'invoiceDate',
+    },
+    {
+      Header: 'Vrsta računa',
+      accessor: 'invoiceType',
+    },
+    {
+      Header: 'Stanje',
+      accessor: 'invoiceState',
+    },
+    {
+      Header: 'Ukupan iznos',
+      accessor: 'invoiceTotal',
+    },
+  ];
+
+  const path = 'invoice';
+
+  let invoicesList;
+  const noInvoices = <p>Kupac nema izdanih računa.</p>;
+
+  console.log(partnerInvoices);
+
+  partnerInvoices.length !== 0
+    ? (invoicesList = (
+        <DefaultTable
+          path={path}
+          appData={partnerInvoices}
+          tableColumns={COLUMNS}
+        />
+      ))
+    : (invoicesList = noInvoices);
 
   return (
     <PartnerStyle>
@@ -29,11 +82,11 @@ const Partner = (props) => {
       <p>{partner.address}</p>
       <p>{partner.dateAdded}</p>
 
-      <MainFooter>
-        {/* <Link to="/add-invoice">
-          <Button>Dodaj novi račun</Button>
-        </Link> */}
+      <h3>Izdani računi:</h3>
 
+      {invoicesList}
+
+      <MainFooter>
         <Button
           onClick={() => {
             history.goBack();
